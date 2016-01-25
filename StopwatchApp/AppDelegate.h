626d8df5a44673255bc9e50e49895874a0a48445
Haruka <Haruka@haruchan-mac.local>
2016-01-25 22:25:29 +0900

    Initial Commit

---
+++ StopwatchApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StopwatchApp
+//
+//  Created by Haruka on 2016/01/25.
+//  Copyright © 2016年 Haruka.Shida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StopwatchApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StopwatchApp
+//
+//  Created by Haruka on 2016/01/25.
+//  Copyright © 2016年 Haruka.Shida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
