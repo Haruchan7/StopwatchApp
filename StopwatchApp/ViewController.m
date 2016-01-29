6adb4d7578e7cee78efb04e1f20d76457400c495
Haruka <Haruka@haruchan-mac.local>
2016-01-30 02:04:30 +0900

    とりあえず。

--- StopwatchApp/ViewController.m
+++ StopwatchApp/ViewController.m
@@ -12,9 +12,10 @@
     UILabel *timelabel;
     UIImageView *background;
     NSTimer *stopwatch;
-    NSInteger countNumber;
     UIButton *startstop;
-    
+    NSInteger min;
+    NSInteger sec;
+
     //start/stop切り替え条件用
     BOOL condition;
 }
@@ -31,11 +32,11 @@
     timelabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 300, 100)];
     timelabel.center = CGPointMake(160, 284);
     timelabel.font = [UIFont fontWithName:@"Chalkboard Bold" size:30];
-    timelabel.text = @"00";
+    timelabel.text = @"00:00";
     timelabel.textAlignment = NSTextAlignmentCenter;
     timelabel.backgroundColor = [UIColor clearColor];
     [self.view addSubview:timelabel];
-    
+
     startstop = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     startstop.frame = CGRectMake(0, 0, 100, 50);
     startstop.center = CGPointMake(160, 400);
@@ -56,7 +57,7 @@
 
 -(void)switchButton:(id)sender{
     if (condition == true){
-        [stopwatch fire];
+        stopwatch = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(countSec:) userInfo:nil repeats:YES];
         [startstop setTitle:@"STOP" forState:UIControlStateNormal];
         condition = false;
     }else if(condition == false){
@@ -66,49 +67,39 @@
     }
 }
 
-
 -(void)countSec:(id)sender{
-    NSLog(@"カウント開始");
-    NSInteger *min;
-    NSInteger *sec;
-//    for (min = 0; sec < 60; sec++){
-    timelabel.text = [NSString stringWithFormat:@"%ld : %ld",min, sec];
-            }
-    //    if(condition == 0){
-//        [stopwatch fire];
-//       [timelabel setText:]
-//        condition = 1;
-//    }else{
-//        [stopwatch invalidate];
-//        condition = 0;
-//    }
-//    for (countNumber = 0; i<60; i++) {
-//        [timelabel setText:];
-//    }
-//}
+    NSLog(@"カウントしています");
+    sec += 1;
+    timelabel.text = [NSString stringWithFormat:@"%02d : %02d",min,sec];
 
+    if (sec%60 == 0){
+        min += 1;
+        sec = 0;
+        timelabel.text = [NSString stringWithFormat:@"%02d : %02d",min,sec];
+    }else if(sec%60 != 0){
+        timelabel.text = [NSString stringWithFormat:@"%02d : %02d",min,sec];
+    }
+}
 
 - (void)resetCount:(id)sender{
-    timelabel.text = @"00:00";
+    min = 0;
+    sec = 0;
+    timelabel.text = [NSString stringWithFormat:@"%02d : %02d",min,sec];
     [self.view addSubview:timelabel];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    sec = 0;
+    min = 99;
+    condition = true;
     [self setupParts];
-    
-    countNumber = 0;
-    
-    //タイマーインスタンスを作成
-    stopwatch = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(countSec:) userInfo:nil repeats:YES];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-
-    
 }
 
 @end
